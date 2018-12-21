3d888a657c5e06d70a7701010a7be8f93f11f765
Ibrahim Butt <ibrahimbutt@me.com>
2018-12-21 01:03:17 +0000

    Change Subsequent.test to return word on passing
    Co-authored-by: Nkhil <nkhilv@gmail.com>

--- lib/one.rb
+++ lib/one.rb
@@ -7,23 +7,36 @@
 # For example, given the input of S = "abppplee" and D = {"able", "ale", "apple", "bale", "kangaroo"} the correct output would be "apple"
 
 module Subsequence
-
   def self.test(string, word)
     # (String, String) -> String
     # returns word if subsequent
 
-    'able'
+    result = word.chars.map.with_index do |letter, i|
+      if string.include? letter
+        previous_letter = i != 0 ? word.chars[i - 1] : false
+        if previous_letter
+          current_letter_index = string.rindex(letter)
+          previous_letter_index = string.index(word.chars[i - 1])
+          letter if current_letter_index > previous_letter_index
+        else
+          letter
+        end
+      else
+        break
+      end
+    end
+
+    result.join if result
   end
 
   def self.all(word, arr)
     # (String, Array) -> Array
     # returns an array of passing strings
   end
-  
+
   def self.longest(word, arr)
     # (String, Array) -> String
     # returns longest subsequent string
-
     'apple'
   end
 end
