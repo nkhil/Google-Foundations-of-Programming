30cc799dc0a6594917bd69d84cb821ade21d73dd
Ibrahim Butt <ibrahimbutt@me.com>
2018-12-21 01:00:28 +0000

    Remove unnecessary context from tests

--- spec/one_spec.rb
+++ spec/one_spec.rb
@@ -1,17 +1,15 @@
 require 'one'
 
 describe Subsequence do
-  describe '.result'
-  context "should return `apple`." do
-    it "returns 'apple'" do
-      expect(Subsequence.longest).to eq 'apple'
+  describe '.check' do
+    it "should return word if it's a subsequent" do
+      expect(Subsequence.test('abppplee', 'apple')).to eq 'apple'
     end
   end
 
-  describe '.check'
-  context "should return `able`." do
-    it "returns 'able'" do
-      expect(Subsequence.test('able')).to eq 'able'
+  describe '.result' do
+    it "should return 'apple'" do
+      expect(Subsequence.longest('abppplee', ['able', 'ale', 'apple'])).to eq 'apple'
     end
   end
 end
